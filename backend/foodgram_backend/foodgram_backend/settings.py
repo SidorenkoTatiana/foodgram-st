342e636e994ec6ca5bb434c12c5e420bc42de58e
Tatiana Sidorenko <sidorenkotatana@gmail.com>
2025-01-28 03:56:50 +0300

    Add all

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -12,20 +12,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.core.management.utils import get_random_secret_key
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8+i&+#0#_n+d@8y#=5y=)q_o*ow(8lhg97ply%yd-t-022#1_n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+DEBUG = os.getenv('DEBUG') == 'True'
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 AUTH_USER_MODEL = 'users.User'
 
