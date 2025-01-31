5ded384bbf427547a5f97431ce6a5ab7080930ef
SidorenkoTatiana <sidorenkotatana@gmail.com>
2025-01-31 16:39:00 +0300

    Add

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -17,7 +17,7 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = 'False'
 ALLOWED_HOSTS = ['192.168.122.80', '127.0.0.1', 'localhost']
 
 AUTH_USER_MODEL = 'users.User'
