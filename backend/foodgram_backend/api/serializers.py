3ac15b59546bc1c72e8376eabfd286c4cae7ada4
Tatiana Sidorenko <sidorenkotatana@gmail.com>
2025-02-11 01:00:11 +0300

    change

--- backend/foodgram_backend/api/serializers.py
+++ backend/foodgram_backend/api/serializers.py
@@ -101,21 +101,21 @@ class RecipeListSerializer(serializers.ModelSerializer):
     def get_is_favorited(self, obj):
         request = self.context.get('request')
         return (
-            request.user.is_authenticated and
-            models.FavoriteRecipe.objects.filter(
-                user=request.user,
-                recipe=obj
-            ).exists()
+                request.user.is_authenticated and
+                (models.FavoriteRecipe.objects.filter(
+                    user=request.user,
+                    recipe=obj
+                ).exists())
         )
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
         return (
             request.user.is_authenticated and
-            models.ShoppingCart.objects.filter(
+            (models.ShoppingCart.objects.filter(
                 user=request.user,
                 recipe=obj
-            ).exists()
+            ).exists())
         )
 
 
