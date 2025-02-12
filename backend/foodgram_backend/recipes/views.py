35bb46a97d3cb32d920613d91f0cb5d6df61eb2f
Tatiana Sidorenko <sidorenkotatana@gmail.com>
2025-02-12 21:13:58 +0300

    change forth

--- backend/foodgram_backend/recipes/views.py
+++ backend/foodgram_backend/recipes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
+
+from .models import Recipe
 
 
 def short_link_redirect(request, pk):
+    get_object_or_404(Recipe, pk=pk)
     return redirect(f'/recipes/{pk}')
